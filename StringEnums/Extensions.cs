f310f84e7249349eb32130ed7e90af654a8117e3
DavidS <david@tan.com>
2021-06-21 14:17:42 +0800

    polish

--- StringEnums/Extensions.cs
+++ StringEnums/Extensions.cs
@@ -5,18 +5,22 @@ namespace StringEnums
 {
     public static class Extensions
     {
-        public static T? ToStringEnum<T>(this string str) where T : StringEnum<T>, new() =>
-            StringEnum<T>.ToStringEnum(str);
+        public static T? ToStringEnum<T>(this string str) where T : StringEnum<T>, new()
+        {
+            return StringEnum<T>.ToStringEnum(str);
+        }
 
-        public static bool IsStringEnum(this Type type) =>
-            IsStringEnum(type.GetTypeInfo());
+        public static bool IsStringEnum(this Type type)
+        {
+            return IsStringEnum(type.GetTypeInfo());
+        }
 
         public static bool IsStringEnum(this TypeInfo typeInfo)
         {
             if (typeInfo == null)
                 throw new ArgumentNullException(nameof(typeInfo));
 
-            var baseType = typeInfo.BaseType;
+            Type? baseType = typeInfo.BaseType;
             if (baseType == null || !baseType.GetTypeInfo().IsGenericType)
                 return false;
 
--- StringEnums/StringEnum.cs
+++ StringEnums/StringEnum.cs
@@ -2,27 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
-/*
-Self Referencing Generic Constraint
 
-Notice that the generic constraint is self-referencing. This is a pattern that Eric Lippert discourages:
-Yes it is legal, and it does have some legitimate uses. I see this pattern rather a lot(**). However, I personally don’t like it and I discourage its use.
-This is a C# variation on what’s called the Curiously Recurring Template Pattern in C++, and I will leave it to my betters to explain its uses in that language.
-Essentially the pattern in C# is an attempt to enforce the usage of the CRTP.
-…snip…
-So that’s one good reason to avoid this pattern: because it doesn’t actually enforce the constraint you think it does.
-…snip…
-The second reason to avoid this is simply because it bakes the noodleof anyone who reads the code.
-https://haacked.com/archive/2012/09/30/primitive-obsession-custom-string-types-and-self-referencing-generic-constraints.aspx/
-*/
 namespace StringEnums
 {
+    // class with self-referencing generic constraint
     public abstract class StringEnum<T> where T : StringEnum<T>, new()
     {
-        static StringEnum() => RuntimeHelpers.RunClassConstructor(typeof(T).TypeHandle); // static ctor
+        static StringEnum() // static ctor
+        {
+            RuntimeHelpers.RunClassConstructor(typeof(T).TypeHandle);
+        }
 
         private static Dictionary<string, T> Constants = new();
-        public static void SetStringComparer(StringComparer comparer) => Constants = new Dictionary<string, T>(Constants, comparer);
+        public static void SetStringComparer(StringComparer comparer)
+        {
+            Constants = new Dictionary<string, T>(Constants, comparer);
+        }
 
         public static IList<T> ToStringEnums()
         {
@@ -32,12 +27,21 @@ namespace StringEnums
 
         private string[] Strings = Array.Empty<string>();
 
-        public IList<string> ToStrings() => Strings.ToList(); // return a copy
+        public IList<string> ToStrings()
+        {
+            return Strings.ToList(); // return a copy
+        }
 
-        public override string ToString() => Strings.FirstOrDefault() ?? "";
+        public override string ToString()
+        {
+            return Strings.FirstOrDefault() ?? "";
+        }
 
-        protected static T Create(params string[] strings) =>
-            Add(strings) ?? throw new ArgumentException($"StringEnum<{typeof(T).Name}>.Create(): string value in {(string.Join(",", strings))} already exists.");
+        protected static T Create(params string[] strings)
+        {
+            return Add(strings) ?? 
+                throw new ArgumentException($"StringEnum<{typeof(T).Name}>.Create(): string value in {(string.Join(",", strings))} already exists.");
+        }
 
         public static T? Add(params string[] strings)
         {
@@ -51,7 +55,7 @@ namespace StringEnums
                 if (strings.Where(str => Constants.ContainsKey(str)).Any())
                     return null; // null indicates that no StringEnum was added because at least one of the string arguments already exists.
 
-                var constant = new T { Strings = strings };
+                T constant = new() { Strings = strings };
 
                 foreach (var str in strings)
                     Constants.Add(str, constant);
